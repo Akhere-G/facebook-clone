17e3e573e972aeb03c13a5b2e9485b74cbc9e465
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2020-12-12 05:26:10 +0000

    fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -53,27 +53,21 @@ export default function Header() {
           <Avatar src={user.photoURL} />
           <h4>{user.displayName}</h4>
         </div>
-        <div className={styles.headerRightIcons}></div>
-        <IconButton>
-          <AddIcon />
-        </IconButton>
-        <IconButton>
-          <ForumIcon />
-        </IconButton>
-        <IconButton>
-          <NotificationsActiveIcon />
-        </IconButton>
-        <IconButton>
-          <ExpandMoreIcon />
-        </IconButton>
+        <div className={styles.headerRightIcons}>
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+          <IconButton>
+            <ForumIcon />
+          </IconButton>
+          <IconButton>
+            <NotificationsActiveIcon />
+          </IconButton>
+          <IconButton>
+            <ExpandMoreIcon />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
 }
-
-/*"https://upload.wikimedia.org/
-wikipedia/commons/thumb/5/51/
-Facebook_f_logo_%282019%29.svg/
-1200px-Facebook_f_logo_%282019%29.svg.png
-"
-*/
